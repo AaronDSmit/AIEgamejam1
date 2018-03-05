88404c7d239954f4969a85ad861508d1fe2a9202
AIE\s171041 <smitaaron1@gmail.com>
2018-03-05 16:07:20 +1100

    Added Player controller

---
+++ GameJam_01/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private int maxRubbish;
+
+    [SerializeField]
+    private float dockTime;
+
+    private int currentRubbish;
+
+    private Player movement;
+
+    private bool canDock = false;
+
+    private Manager manager;
+
+    private void Awake()
+    {
+        manager = GameObject.FindObjectOfType<Manager>();
+    }
+
+    public void Reset()
+    {
+        canDock = false;
+    }
+
+    private void Update()
+    {
+        if (canDock && Input.GetKeyDown(KeyCode.A))
+        {
+            // movement.setMove(false);
+            canDock = false;
+            manager.DisplayDockPrompt(false);
+
+            Invoke("ReleaseDock", dockTime);
+        }
+    }
+
+    private void ReleaseDock()
+    {
+        // movement.setMove(true);
+    }
+
+    public void CanDock(bool _canDock)
+    {
+        canDock = _canDock;
+    }
+
+    private IEnumerator DrainRubbish()
+    {
+        yield return null;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Rubbish"))
+        {
+            Destroy(collision.transform.gameObject);
+            currentRubbish++;
+
+            currentRubbish = Mathf.Clamp(currentRubbish, 0, maxRubbish);
+        }
+    }
+}
